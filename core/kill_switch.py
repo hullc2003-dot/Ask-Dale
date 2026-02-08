376f614389900abafed255b2f8dad98d5740777d
hullc2003-dot <hullc2003@gmail.com>
2026-02-07 22:53:02 -0600

    Delete core directory

--- core/kill_switch.py
+++
@@ -1,9 +0,0 @@
-class KillSwitch:
-    def __init__(self, db):
-        self.db = db
-
-    def is_enabled(self) -> bool:
-        flag = self.db.get_flag("system_enabled")
-        if flag is None:
-            return True
-        return bool(flag)

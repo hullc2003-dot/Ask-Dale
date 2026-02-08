be74c35c0cc7c5669c43ec6b023ed5507001ef95
hullc2003-dot <hullc2003@gmail.com>
2026-02-07 22:53:17 -0600

    Delete learning directory

--- learning/learning_governance.py
+++
@@ -1,5 +0,0 @@
-def is_proposal_safe(proposal_text: str, persona_id: str, db) -> bool:
-    # Placeholder: run through safety rules, boundaries, maybe a small model
-    # For now, just a stub that always returns True
-    return True
-

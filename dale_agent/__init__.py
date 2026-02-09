a880a263aa7b098bcdb2e645fa2db11936bd44c5
hullc2003-dot <hullc2003@gmail.com>
2026-02-09 12:20:44 -0600

    Update and rename __init__ to __init__.py

---
+++ dale_agent/__init__.py
@@ -0,0 +1,16 @@
+from .dale import DaleAgent
+from .junk_drawer_processor import JunkDrawerProcessor
+from .gap_analyzer import GapAnalyzer
+from .improvement_engine import ImprovementEngine
+from .feedback_memory import FeedbackMemory
+from .content_writer import ContentWriter
+
+__all__ = [
+    "DaleAgent",
+    "JunkDrawerProcessor",
+    "GapAnalyzer",
+    "ImprovementEngine",
+    "FeedbackMemory",
+    "ContentWriter",
+]
+

30d17c40044d68646f3dadbffc9866b8acaaced8
hullc2003-dot <hullc2003@gmail.com>
2026-02-14 20:01:49 -0600

    Update __init__.py

--- dale_agent/__init__.py
+++ dale_agent/__init__.py
@@ -4,3 +4,9 @@ proveider.py
 self_improving_agent.py
 openrouter_client.py
 junk_drawer_proccesor.py
+self_improving_agent.py
+strategy_writer.py
+gap_analyzer.py
+feedback_memory.py
+agent_strategy.md
+

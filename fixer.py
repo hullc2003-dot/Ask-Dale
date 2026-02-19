f55a04ad45a28ba108885f87c68516ad1ca2f358
hullc2003-dot <hullc2003@gmail.com>
2026-02-19 14:25:00 -0600

    Update fixer.py

--- fixer.py
+++ fixer.py
@@ -12,7 +12,6 @@ import operator
 
 # LangChain and LangGraph imports
 from langchain_groq import ChatGroq
-from langchain.agents import AgentExecutor
 from langchain.prompts import PromptTemplate
 from langchain.tools import Tool
 from langchain.memory import ConversationBufferMemory

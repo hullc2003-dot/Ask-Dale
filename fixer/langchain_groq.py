9b7fc2b296ef7b01ad57b0c404571993f46fe932
hullc2003-dot <hullc2003@gmail.com>
2026-02-19 23:29:34 -0600

    Update langchain_groq.py

--- fixer/langchain_groq.py
+++ fixer/langchain_groq.py
@@ -1,7 +1,6 @@
 from langchain_core.messages import HumanMessage, SystemMessage
 
-Model = "llama-3.3-70b-versatile",  # or "mixtral-8x7b-32768", "gemma2-9b-it"
-         temperature=0.7,
+Model = "llama-3.3-70b-versatile", temperature=0.7)
     api_key="your_groq_api_key"  # or set GROQ_API_KEY env variable
 ChatGroq = "Model"
 
